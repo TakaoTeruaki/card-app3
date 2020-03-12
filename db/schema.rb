1624cb60fb1f33859b58d079637c1ca221ed9fdc
TakaoTeruaki <k.teru.827@gmail.com>
2020-03-12 19:49:44 +0900

    ビュー調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_121253) do
+ActiveRecord::Schema.define(version: 2020_03_12_015117) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "hospital"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_03_10_121253) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "number"
+    t.text "subject"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
